85d003243884ddaf075fcc94b522449200ef7f15
Danny Willems <contact@danny-willems.be>
2016-11-12 21:51:06 +0100

    Scala 2.11.8: downlod from official website and extract it in home.

--- scala.sh
+++ scala.sh
@@ -1,3 +1,4 @@
 # Install scala.
 
-sudo apt-get install scala
+wget -qO- http://downloads.lightbend.com/scala/2.11.8/scala-2.11.8.tgz | tar -xvzf -C ~/.scala-2.11.8
+ln -sf ~/.scala-2.11.8 ~/.scala
