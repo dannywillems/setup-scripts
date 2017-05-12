82deb9f79374e6f8a5b1f4c37a036d784ca10fe7
Danny Willems <danny@b2c2.net>
2017-05-12 08:09:59 +0000

    Update tmux.sh with dependencies.

--- tmux.sh
+++ tmux.sh
@@ -1,9 +1,10 @@
 mkdir -p $HOME/.compile/
 mkdir -p $HOME/.install-compiled/tmux
 
+sudo apt-get install libevent-dev pkg-config autoconf
 git clone https://github.com/tmux/tmux $HOME/.compile/tmux
 cd $HOME/.compile/tmux/
 sh autogen.sh
 ./configure --prefix=$HOME/.install-compiled/tmux
-make -j 4
+make
 make install
