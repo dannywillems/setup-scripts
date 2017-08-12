98a5fedab8182ebfbe75da8f530446462b91f45a
Danny Willems <contact@danny-willems.be>
2017-08-12 16:40:27 +0100

    Update opam instructions.

--- opam.sh
+++ opam.sh
@@ -1,8 +1,8 @@
 set -o nounset                              # Treat unset variables as an error
 
-git clone https://github.com/ocaml/opam $HOME/.compile/opam
-cd $HOME/.compile/opam
-./configure --prefix=$HOME/.install-compiled/opam
+git clone https://github.com/ocaml/opam $HOME/.source/opam
+cd $HOME/.source/opam
+./configure --prefix=$HOME/.bin/opam
 make lib-ext
 make -j 4
 make install
