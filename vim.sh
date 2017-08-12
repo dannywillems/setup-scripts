8eff0b8b8663f90cc30c6bfc779660a5b575dde7
Danny Willems <contact@danny-willems.be>
2017-08-12 16:43:35 +0100

    vim: update source and bin directories.

--- vim.sh
+++ vim.sh
@@ -1,8 +1,8 @@
-mkdir -p $HOME/.compile/vim
-mkdir -p $HOME/.install-compiled/vim
+mkdir -p $HOME/.source/vim
+mkdir -p $HOME/.bin/vim
 
-git clone https://github.com/vim/vim $HOME/.compile/vim
-cd $HOME/.compile/vim
+git clone https://github.com/vim/vim $HOME/.source/vim
+cd $HOME/.source/vim
 ./configure --with-features=huge \
             --enable-multibyte \
             --enable-rubyinterp=yes \
@@ -10,6 +10,6 @@ cd $HOME/.compile/vim
             --enable-python3interp=yes \
             --enable-perlinterp=yes \
             --enable-luainterp=yes \
-            --enable-gui=gtk2 --enable-cscope --prefix=$HOME/.install-compiled/vim
+            --enable-gui=gtk2 --enable-cscope --prefix=$HOME/.bin/vim
 make -j 8
 make install
